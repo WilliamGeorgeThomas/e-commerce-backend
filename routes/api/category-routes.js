8fe880c57c264118c8be011a071eb61ce8c59585
WGT <williamgeorgethomas@gmail.com>
2023-01-24 18:34:34 -0600

    seeded tables and edited first routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,36 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
+  const data = await Category.findAll({
+    include: [Product],
+  });
+
+  return res.json(data);
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
+  const data = await Category.findOne({
+    where: { id: req.params.id },
+  });
   // be sure to include its associated Products
+  return res.json(data);
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
